c35f59ef920f58fda4eef5b64873672aea70060a
Munch42 <munch4242@gmail.com>
2024-04-11 22:06:13 -0600

    Added Metal Detector

--- src/main/java/ca/munchdev/tutorialmod/item/ModItems.java
+++ src/main/java/ca/munchdev/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package ca.munchdev.tutorialmod.item;
 
 import ca.munchdev.tutorialmod.TutorialMod;
+import ca.munchdev.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +17,8 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
